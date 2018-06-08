ec97a523f512ec52815f286b805d5694a332cd42
RamFlo <ramsh92@gmail.com>
2018-06-08 08:40:07 -0400

    finished

--- api/controllers/deviceControllers.js
+++ api/controllers/deviceControllers.js
@@ -19,3 +19,31 @@ exports.create_device = function (request, callback) {
 
     });
 };
+
+exports.get_device_risk_by_id = function (request, callback) {
+    Device.find({ 'device_id': request.params.device_id }, 'risk', function (err, dev) {
+        if (err)
+            callback.send(err);
+        callback.json(dev);
+    });
+};
+
+exports.delete_device_by_id = function (request, callback) {
+    Device.remove({ 'device_id': request.params.device_id }, function (err, dev) {
+        if (err)
+            callback.send(err);
+        else if (dev.n === 0)
+            callback.json({ message: 'Could not find specified device' });
+        else
+            callback.json({ message: 'Successfully deleted device' });
+    });
+};
+
+exports.count_devices_by_risk = function (request, callback) {
+    Device.count({ 'risk': request.params.risk }, function (err, counter) {
+        if (err)
+            callback.send(err);
+        else
+            callback.json(counter);
+    });
+};
--- api/routes/deviceRoutes.js
+++ api/routes/deviceRoutes.js
@@ -5,4 +5,11 @@ module.exports = function (app) {
     app.route('/devices')
         .get(devices.list_all_devices)
         .post(devices.create_device);
+
+    app.route('/devices/:device_id')
+        .get(devices.get_device_risk_by_id)
+        .delete(devices.delete_device_by_id);
+
+    app.route('/devices/count_by_risk/:risk')
+        .get(devices.count_devices_by_risk);
 };
